63ed552e271c383b3c4cb4b70705faf44e9b8be2
Nontchai Tantiponsawas <nontchai@Nontchais-MacBook-Pro.local>
2017-06-12 01:08:52 +0700

    Add search object

--- src/TopNavigation.js
+++ src/TopNavigation.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './topNavigation.css'
 import backIcon from './asset/back.png'
 import cancelIcon from './asset/cancel.png'
+import starIcon from './asset/star.png'
 
 export default class TopNavigation extends Component {
   render () {
@@ -17,6 +18,26 @@ export default class TopNavigation extends Component {
 	  </div>
         </div>
         <div className='contentContainer'>
+	  <div className='contentHeader'>Result by search 200 attractions</div>
+	  <div className='resultContainer'>
+            <div className='resultBox'>
+	      <div className='resultImage'></div>
+	      <div className='result'>
+	        <div className='resultName'>Chalong Temple</div>
+	        <div className='resultZone'>Phuket Town</div>
+	        <div className='resultStar'>
+	          <img src={starIcon} />
+	          <img src={starIcon} />
+	          <img src={starIcon} />
+	          <img src={starIcon} />
+	          <img src={starIcon} />
+	        </div>
+	      </div>
+	      <div className='resultDistance'>
+	        0.5 km
+	      </div>
+	    </div>
+          </div>
         </div>
       </div>
     )
